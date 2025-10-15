e23322b5faaddc53c3b3d4100f56774ec00f82f9
Onur Emre Çakır <onur23emre@hotmail.com>
2025-10-15 18:58:51 +0300

    Update 3_bit_adder.v

--- 3_bit_adder.v
+++ 3_bit_adder.v
@@ -13,3 +13,24 @@ wire [3:0] sum  = num1 + num2;
 assign led = (select_display == 1'b1) ? {num2, num1} : {4'b0000, sum};
 
 endmodule
+/* ucf
+
+NET "led<7>" LOC = "g1" ; 
+NET "led<6>" LOC = "p4" ; 
+NET "led<5>" LOC = "n4" ; 
+NET "led<4>" LOC = "n5" ; 
+NET "led<3>" LOC = "p6" ; 
+NET "led<2>" LOC = "p7" ; 
+NET "led<1>" LOC = "m11" ; 
+NET "led<0>" LOC = "m5" ; 
+
+NET "sw<7>" LOC = "n3"; 
+NET "sw<6>" LOC = "e2"; 
+NET "sw<5>" LOC = "f3"; 
+NET "sw<4>" LOC = "g3"; 
+NET "sw<3>" LOC = "b4"; 
+NET "sw<2>" LOC = "k3"; 
+NET "sw<1>" LOC = "l3"; 
+NET "sw<0>" LOC = "p11"; 
+
+*/
